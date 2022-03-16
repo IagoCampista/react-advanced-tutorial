1a0e2d4f3630b1fe9e5e492d6a6cf531bee46659
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-15 21:12:00 -0300

    UseState counter example
    How useState works
    a simple example made by me to test me using useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+   const [counter, setCounter] = useState(0);
+
+   const increment = () => {
+      setCounter(counter + 1);
+   };
+   const decrement = () => {
+      setCounter(counter - 1);
+   };
+   const reset = () => {
+      setCounter(0);
+   };
+
+   return (
+      <React.Fragment>
+         <h1>{counter}</h1>
+         <button type="button" className="btn" onClick={decrement}>
+            Decrement
+         </button>
+         <button type="button" className="btn" onClick={reset}>
+            Reset
+         </button>
+         <button type="button" className="btn" onClick={increment}>
+            Increments
+         </button>
+      </React.Fragment>
+   );
 };
 
 export default UseStateBasics;
