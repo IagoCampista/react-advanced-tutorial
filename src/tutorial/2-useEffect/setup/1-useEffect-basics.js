89056a43d0c7a0b88f54a2b313249d64e7a7266c
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-20 17:16:41 -0300

    useEffect test

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+   const [value, setValue] = useState(0);
+   const [value1, setValue1] = useState(0);
+   const [resultValue, setResultValue] = useState(0);
+   const [resultValue1, setResultValue1] = useState(0);
+
+   useEffect(() => {
+      setResultValue(value);
+   }, [value1]);
+   return (
+      <>
+         <h2>useEffect Basics</h2>
+         <h3>{value}</h3>
+         <button className="btn" onClick={() => setValue(value + 1)}>
+            Increment
+         </button>
+         <button className="btn" onClick={() => setValue(0)}>
+            reset
+         </button>
+         <p></p>
+         <hr></hr>
+
+         <h3>{value1}</h3>
+         <button className="btn" onClick={() => setValue1(value1 + 1)}>
+            Increment
+         </button>
+         <button className="btn" onClick={() => setValue1(0)}>
+            reset
+         </button>
+         <p></p>
+         <hr></hr>
+         <h3>Result 0: {resultValue}</h3>
+         <h3>Result 1: {resultValue1}</h3>
+      </>
+   );
 };
 
 export default UseEffectBasics;
