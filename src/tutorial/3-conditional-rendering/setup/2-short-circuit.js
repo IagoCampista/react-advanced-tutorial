1b8ed09e31ee73d8ea2247a24c4fa931ad11eae1
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-21 18:48:34 -0300

    short circuit evalluation tested
    All variations noted to the Notions React Studies Page

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
-const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+// const firstValue = text || 'hello world';
+// const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+const ShortCircuit = () => {
+   const [text, setText] = useState("hello"); // valor inicialmente truthy por conter algo
+
+   return (
+      <>
+         <h1>{text && "hello world 1"}</h1>{" "}
+         {/*retorna a string hello world 1 pelo primeiro ser truthy*/}
+         <h1>{!text && "hello world 2"}</h1>
+         {/*retorna nada*/}
+      </>
+   );
 };
 
 export default ShortCircuit;
