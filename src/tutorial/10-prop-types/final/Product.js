8f1b35711841600b6c8aedba8608fc1ca61cbd1f
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-29 16:59:35 -0300

    PropTypes comments added and router-dom intallesd
    PropTypes comments added and
    npm install react-router-dom command done

--- src/tutorial/10-prop-types/final/Product.js
+++ src/tutorial/10-prop-types/final/Product.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import defaultImage from '../../../assets/default-image.jpeg';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 const Product = ({ image, name, price }) => {
-  const url = image && image.url;
-  return (
-    <article className='product'>
-      <img src={url || defaultImage} alt={name || 'default name'} />
-      <h4>{name}</h4>
-      <p>${price || 3.99}</p>
-    </article>
-  );
+   const url = image && image.url;
+   // The line above checks if the image exists, if it exists it returns the url
+   //If the image does not exist, then it returns nothing,
+   //So it just uses the url IF there is an image
+   return (
+      <article className="product">
+         <img src={url || defaultImage} alt={name || "default name"} />
+         {/* The OR operator here checks if there is an url, if it exists it uses the url, if not it uses the Default Image*/}
+         <h4>{name}</h4>
+         <p>${price || 3.99}</p>
+      </article>
+   );
 };
 
 Product.propTypes = {
-  image: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
+   image: PropTypes.object.isRequired,
+   name: PropTypes.string.isRequired,
+   price: PropTypes.number.isRequired,
 };
+//Here is set that the image object, the name and the price is Required to ths aplication
+// If they are not specified the an Error message is shown to us
+
 // Product.defaultProps = {
 //   name: 'default name',
 //   price: 3.99,
